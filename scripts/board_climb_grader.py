a32f0b0b0cf37cfa6f29c9bfcf93ad1668f53277
ajsquest-cell <ajsquest@gmail.com>
2026-07-12 10:22:26 -0400

    update board climb grader
    used pandas and numpy for math and added 2 bucket easy/hard log regression model and a 3 bucket random forest model

--- scripts/board_climb_grader.py
+++ scripts/board_climb_grader.py
@@ -6,7 +6,9 @@ import random
 
 import numpy as np
 import pandas as pd
+import scipy as sp
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 
@@ -18,12 +20,12 @@ import access_database as adb
 PLACEMENTS_PATH = Path(__file__).resolve().parent / "data" / "tb2.db"
 
 ## Board Constants
-x_min = -24
-x_max = 168
-y_min = 0
-y_max = 156
-board_width = x_max - x_min
-board_height = y_max - y_min
+# x_min = -24
+# x_max = 168
+# y_min = 0
+# y_max = 156
+# board_width = x_max - x_min
+# board_height = y_max - y_min
 
 # Hold ID
 HAND_HOLD_IDS = {1, 2, 3}
@@ -58,8 +60,15 @@ def print_hold_data(hold_data):
         print(f"Hold ID: {hold['hold_id']}, X: {hold['x']}, Y: {hold['y']}, Type: {hold['hold_type']}, Hand: {hold['is_hand_hold']}, Foot: {hold['is_foot_hold']}")
 
 # Extract features from a given frame and angle
-def extract_features_from_frame(angle, frame):
+def extract_features_from_frame(angle, frame, is_nomatch, edge_left, edge_right, edge_bottom, edge_top):
     holds = parse_frame(frame)
+
+    x_min = edge_left
+    x_max = edge_right
+    y_min = edge_bottom
+    y_max = edge_top
+    board_width = x_max - x_min
+    board_height = y_max - y_min
 
     if holds is None:
         raise ValueError("Invalid frame format.")
@@ -76,17 +85,26 @@ def extract_features_from_frame(angle, frame):
         is_foot_hold = h_id in FOOT_HOLD_IDS
         hold_data.append({
             'hold_id': p_id,
+            'hold_type': hold_type,
             'x': coordinate[0],
             'y': coordinate[1],
-            'hold_type': hold_type,
             'is_hand_hold': is_hand_hold,
             'is_foot_hold': is_foot_hold
         })
 
-    # testing
-    # print_hold_data(hold_data)
+    holds_df = pd.DataFrame(hold_data, columns=['hold_id', 'hold_type', 'x', 'y', 'is_hand_hold', 'is_foot_hold'])
 
-    df = pd.DataFrame(hold_data)
+    if holds_df.empty:
+        return None
+
+    hand_holds = holds_df[holds_df['is_hand_hold']]
+    foot_holds = holds_df[holds_df['is_foot_hold']]
+    start_holds = holds_df[holds_df['hold_type'] == 'start']
+    finish_holds = holds_df[holds_df['hold_type'] == 'finish']
+    middle_holds = holds_df[holds_df['hold_type'] == 'middle']
+
+    x = holds_df['x'].to_numpy()
+    y = holds_df['y'].to_numpy()
 
     features = {}
 
@@ -96,66 +114,84 @@ def extract_features_from_frame(angle, frame):
     features['angle'] = angle
     features["angle_squared"] = float(angle) ** 2 if angle is not None else None
     features['num_holds'] = len(holds)
-    features['num_hand_holds'] = len([hold for hold in hold_data if hold['is_hand_hold']])
-    features['num_foot_holds'] = len([hold for hold in hold_data if hold['is_foot_hold']])
-    features['start_holds'] = len([hold for hold in hold_data if hold['hold_type'] == 'start'])
-    features['finish_holds'] = len([hold for hold in hold_data if hold['hold_type'] == 'finish'])
-    features['middle_holds'] = len([hold for hold in hold_data if hold['hold_type'] == 'middle'])
+    features['num_hand_holds'] = len(hand_holds)
+    features['num_foot_holds'] = len(foot_holds)
+    features['start_holds'] = len(start_holds)
+    features['finish_holds'] = len(finish_holds)
+    features['middle_holds'] = len(middle_holds)
+    features['is_nomatch'] = int(is_nomatch == 1)
 
     # Spatial features
-    if hold_data:
-        features['mean_y'] = sum(hold['y'] for hold in hold_data) / len(hold_data)
-        features['std_y'] = (sum((hold['y'] - features['mean_y']) ** 2 for hold in hold_data) / len(hold_data)) ** 0.5
-        features['min_y'] = min(hold['y'] for hold in hold_data)
-        features['max_y'] = max(hold['y'] for hold in hold_data)
-        features['min_x'] = min(hold['x'] for hold in hold_data)
-        features['max_x'] = max(hold['x'] for hold in hold_data)
-        features['range_y'] = features['max_y'] - features['min_y']
-        features['range_x'] = features['max_x'] - features['min_x']
+    features['mean_y'] = float(np.mean(y))
+    features['std_y'] = float(np.std(y)) if len(y) > 1 else 0.0
+    features['std_X'] = float(np.std(x)) if len(x) > 1 else 0.0
+    features['min_y'] = float(np.min(y))
+    features['max_y'] = float(np.max(y))
+    features['min_x'] = float(np.min(x))
+    features['max_x'] = float(np.max(x))
+    features['range_y'] = features['max_y'] - features['min_y']
+    features['range_x'] = features['max_x'] - features['min_x']
 
-        features['start_height'] = min(hold['y'] for hold in hold_data if hold['hold_type'] == 'start') if features['start_holds'] > 0 else None
-        features['finish_height'] = max(hold['y'] for hold in hold_data if hold['hold_type'] == 'finish') if features['finish_holds'] > 0 else None
-        features['height_gained_from_start_to_finish'] = (features['finish_height'] - features['start_height']) if features['start_height'] is not None and features['finish_height'] is not None else None
-    else:
-        features['mean_y'] = 0.0
-        features['std_y'] = 0.0
-        features['min_y'] = 0.0
-        features['max_y'] = 0.0
-        features['min_x'] = 0.0
-        features['max_x'] = 0.0
-        features['range_y'] = 0.0
-        features['range_x'] = 0.0
-        features['start_height'] = None
-        features['finish_height'] = None
-        features['height_gained_from_start_to_finish'] = None
+    features['start_height'] = float(start_holds['y'].min()) if features['start_holds'] > 0 else None
+    features['finish_height'] = float(finish_holds['y'].max()) if features['finish_holds'] > 0 else None
+    features['height_gained'] = (
+        (features['finish_height'] - features['start_height']) 
+        if features['start_height'] is not None and features['finish_height'] is not None else None
+        )
+    features['hold_per_unit_height'] = float(features['num_holds'] / max(features['range_y'], 1)) if features['range_y'] > 0 else 0
 
     # Density features
-    features['box_area'] = features['range_x'] * features['range_y'] if features['range_x'] is not None and features['range_y'] is not None and features['range_x'] > 0 and features['range_y'] > 0 else 1
-    features['hold_density'] = features['num_holds'] / features['box_area'] if features['box_area'] else 0
-    features['hold_density_squared'] = (features['num_holds'] / features['box_area']) ** 2 if features['box_area'] else 0
-    features['hand_hold_density'] = features['num_hand_holds'] / features['box_area'] if features['box_area'] else 0
-    features['foot_hold_density'] = features['num_foot_holds'] / features['box_area'] if features['box_area'] else 0
-    # features['hold_per_unit_height'] = features['num_holds'] / features['height_gained'] if features['height_gained'] else 0
+    features['box_area'] = (
+        float(features['range_x'] * features['range_y'])
+        if features['range_x'] is not None and features['range_y'] is not None and features['range_x'] > 0 and features['range_y'] > 0 else 1
+    )
+    features['hold_density'] = float(features['num_holds'] / features['box_area']) if features['box_area'] else 0
+    features['hand_hold_density'] = float(features['num_hand_holds'] / features['box_area']) if features['box_area'] else 0
 
     # Hand reach features
-    if features['num_hand_holds'] >= 2 and hold_data:
-        hand_pairs = [i for i in range(1, len(hold_data)) if hold_data[i]['is_hand_hold'] and hold_data[i-1]['is_hand_hold']]
-        if hand_pairs:
-            features['mean_hand_reach'] = sum(abs(hold_data[i]['x'] - hold_data[i-1]['x']) for i in hand_pairs) / len(hand_pairs)
-            features['max_hand_reach'] = float(max(abs(hold_data[i]['x'] - hold_data[i-1]['x']) for i in range(1, len(hold_data)) if hold_data[i]['is_hand_hold'] and hold_data[i-1]['is_hand_hold']))
-            features['std_hand_reach'] = (sum((abs(hold_data[i]['x'] - hold_data[i-1]['x']) - features['mean_hand_reach']) ** 2 for i in range(1, len(hold_data)) if hold_data[i]['is_hand_hold'] and hold_data[i-1]['is_hand_hold']) / (features['num_hand_holds'] - 1)) ** 0.5
-        else:
-            features['mean_hand_reach'] = 0.0
-            features['max_hand_reach'] = 0.0
-            features['std_hand_reach'] = 0.0
-        # features['hand_span_y'] = max(hold['y'] for hold in hold_data if hold['is_hand_hold']) - min(hold['y'] for hold in hold_data if hold['is_hand_hold'])
-        # features['hand_span_x'] = max(hold['x'] for hold in hold_data if hold['is_hand_hold']) - min(hold['x'] for hold in hold_data if hold['is_hand_hold'])
+    if features['num_hand_holds'] >= 2:
+        hand_pairs = hand_holds[['x', 'y']].to_numpy()
+        hand_hold_distances = sp.spatial.distance.pdist(hand_pairs)
+        hand_x = hand_holds['x'].to_numpy()
+        hand_y = hand_holds['y'].to_numpy()
+
+        features['mean_hand_reach'] = float(np.mean(hand_hold_distances))
+        features['max_hand_reach'] = float(np.max(hand_hold_distances))
+        features['std_hand_reach'] = float(np.std(hand_hold_distances))
+        features['hand_spread_x'] = float(np.max(hand_x) - np.min(hand_x))
+        features['hand_spread_y'] = float(np.max(hand_y) - np.min(hand_y))
     else:
         features['mean_hand_reach'] = 0.0
         features['max_hand_reach'] = 0.0
         features['std_hand_reach'] = 0.0
-        # features['hand_span_y'] = 0.0
-        # features['hand_span_x'] = 0.0
+        features['hand_spread_x'] = 0.0
+        features['hand_spread_y'] = 0.0
+
+    # Foot reach features
+    if features['num_hand_holds'] > 0 and features['num_foot_holds'] > 0:
+        hand_pairs = hand_holds[['x', 'y']].to_numpy()
+        foot_pairs = foot_holds[['x', 'y']].to_numpy()
+
+        distances = []
+
+        for hand_x, hand_y in hand_pairs:
+            for foot_x, foot_y in foot_pairs:
+                distance = float(np.sqrt((hand_x - foot_x) ** 2 + (hand_y - foot_y) ** 2))
+                distances = np.append(distances, distance)
+        distances = np.array(distances)
+
+        features['mean_hand_foot_reach'] = float(np.mean(distances))
+        features['max_hand_foot_reach'] = float(np.max(distances))
+        features['min_hand_foot_reach'] = float(np.min(distances))
+        features['std_hand_foot_reach'] = float(np.std(distances))
+    else:
+        features['mean_hand_foot_reach'] = 0.0
+        features['max_hand_foot_reach'] = 0.0
+        features['min_hand_foot_reach'] = 0.0
+        features['std_hand_foot_reach'] = 0.0
+
+    # Normalized features
+    
 
     # Other features
     # features['hand_to_foot_ratio'] = features['num_hand_holds'] / features['num_foot_holds'] if features['num_foot_holds'] else float('inf')
@@ -163,12 +199,73 @@ def extract_features_from_frame(angle, frame):
 
     return features
 
+def evaluate_two_bucket_logistic(df, feature_columns, threshold=19):
+    binary_df = df[df["climb_grade"].notna()].copy()
+    binary_df["binary_label"] = (binary_df["climb_grade"] >= threshold).astype(int)
+
+    X = binary_df[feature_columns]
+    y = binary_df["binary_label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = LogisticRegression(max_iter=4000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+
+    print("\n2-Bucket Logistic Regression Results:")
+    print(f'Overall Accuracy: {accuracy:.4f}')
+    print(f'Confusion Matrix:\n{conf_matrix}')
+
+
+def evaluate_bucketed_random_forest(df, feature_columns):
+    bucket_df = df[df["climb_grade"].notna()].copy()
+
+    def bucket_grade(grade):
+        if grade <= 17:
+            return 0
+        elif grade <= 23:
+            return 1
+        else:
+            return 2
+
+    bucket_df["bucket_label"] = bucket_df["climb_grade"].apply(bucket_grade)
+
+    X = bucket_df[feature_columns]
+    y = bucket_df["bucket_label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = RandomForestClassifier(
+        n_estimators=300,
+        max_depth=None,
+        random_state=42,
+        class_weight="balanced"
+    )
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+
+    print("\n3-Bucket Random Forest Results:")
+    print(f'Overall Accuracy: {accuracy:.4f}')
+    print(f'Confusion Matrix:\n{conf_matrix}')
+
 # Testing
 def main():
     difficulty_grades = adb.extract_difficulty_grades()
 
     # number of climbs and offset
-    count = 1000
+    count = 5000
     offset = random.randint(0, adb.get_table_size("climbs") - count)
 
     # Fetch rows from the database
@@ -182,14 +279,28 @@ def main():
             'climb_id': row['uuid'],
             'angle': adb.fetch_angle(row['uuid']),
             'frames': row['frames'],
+            'is_nomatch': row['is_nomatch'],
             'edge_left': row['edge_left'],
             'edge_right': row['edge_right'],
             'edge_bottom': row['edge_bottom'],
             'edge_top': row['edge_top'],
         }
+
         stats = adb.fetch_climb_stats(record['climb_id'])
         record['climb_grade'] = int(stats['display_difficulty']) if stats and stats.get('display_difficulty') is not None else None
-        features = extract_features_from_frame(record['angle'], record['frames'])
+        
+        features = extract_features_from_frame(
+            record['angle'], 
+            record['frames'], 
+            record['is_nomatch'], 
+            record['edge_left'], 
+            record['edge_right'], 
+            record['edge_bottom'], 
+            record['edge_top'])
+        
+        if features is None:
+            continue
+
         record.update(features)
         climb_data.append(record)
         # Testing
@@ -200,34 +311,58 @@ def main():
     # Remove rows with missing features
     df = df[df["climb_grade"].notna()]
 
-    # remove rows with outliers in features or difficulty
-    # add code here
-    
+    feature_columns = [
+        "angle", 
+        "angle_squared", 
+        "num_holds", 
+        "height_gained", 
+        "hold_density", 
+        "mean_hand_reach", 
+        "max_hand_reach", 
+        "std_hand_reach", 
+        "hold_per_unit_height", 
+        "is_nomatch", 
+        "mean_hand_foot_reach", 
+        "max_hand_foot_reach", 
+        "min_hand_foot_reach", 
+        "std_hand_foot_reach"
+        ]
     # Fill NaN values in features with 0
-    feature_columns = ["angle", "angle_squared", "num_holds", "height_gained_from_start_to_finish", "hold_density", "mean_hand_reach", "max_hand_reach", "std_hand_reach"]
     df[feature_columns] = df[feature_columns].fillna(0)
 
-    X = df[feature_columns]
-    y = df["climb_grade"]
+    # Keep the existing classifier on the original 10-28 grade range.
+    classification_df = df[(df["climb_grade"] >= 10) & (df["climb_grade"] <= 28)].copy()
+    X_class = classification_df[feature_columns]
+    y_class = classification_df["climb_grade"]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_class_train, X_class_test, y_class_train, y_class_test = train_test_split(
+        X_class, y_class, test_size=0.2, random_state=42
+    )
 
-    model = LogisticRegression(max_iter=len(climb_data))
-    model.fit(X_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=300,
+        max_depth=None,
+        random_state=42,
+        class_weight="balanced"
+    )
+    model.fit(X_class_train, y_class_train)
 
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    
+    y_class_pred = model.predict(X_class_test)
+    accuracy = accuracy_score(y_class_test, y_class_pred)
+
     # Calculate within buckets
-    within_1 = np.mean(np.abs(y_test - y_pred) <= 1)
-    within_2 = np.mean(np.abs(y_test - y_pred) <= 2)
+    within_1 = np.mean(np.abs(y_class_test - y_class_pred) <= 1)
+    within_2 = np.mean(np.abs(y_class_test - y_class_pred) <= 2)
 
-    conf_matrix = confusion_matrix(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_class_test, y_class_pred)
 
     print(f'Exact Accuracy: {accuracy:.4f}')
     print(f'Within 1 Grade Accuracy: {within_1:.4f}')
     print(f'Within 2 Grades Accuracy: {within_2:.4f}')
     print(f'Confusion Matrix:\n{conf_matrix}')
+
+    evaluate_two_bucket_logistic(df, feature_columns)
+    evaluate_bucketed_random_forest(df, feature_columns)
 
 
 if __name__ == "__main__":
